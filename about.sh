1b18089946e395656b23e113270b955488e9bc43
fmwr <91626229+fmwr@users.noreply.github.com>
2021-09-30 02:00:16 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+blue='\e[1;34m'
+NC='\e[0m'
+clear
+
+echo -e "================================================="
+echo -e "#     Premium Auto Script By @Mawarx86          #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks to  :                                  #"
+echo -e "#-----------------------------------------------#"
+echo -e "# GOD                                           #"
+echo -e "# My Family                                     #"
+echo -e "# My Friends                                    #"
+echo -e "# Horass   Wildy   Rascom   Plankton            #"
+echo -e "# William   Sweetie   Pancho   FastSSH          #"
+echo -e "# Google                                        #"
+echo -e "# Admin Portal & White Vps                      #"
+echo -e "# And many more that I can't to say here        #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Dev        : MawarX86                         #"
+echo -e "# Telegram   : t.me/MawarX86                    #"
+echo -e "# Whatsapp   : wa.me/6285876948650              #"
+echo -e "# Email      : mawarx86@gmail.com               #"
+echo -e "================================================="
+
